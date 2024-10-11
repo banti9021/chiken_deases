4f2304e562ae86080bd6553021b6d3ee96bdd5ce
banti9021 <nbanti943@gmail.com>
2024-10-11 10:50:57 +0530

    Your commit message

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
-from src.cnnClassifier import logger
-logger.info("we")
+from cnnClassifier import logger
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_1data_ingestion import DataIngestionTrainingPipeline
+#from src.cnnClassifier.pipeline.1data_ingestion import DataIngestionTrainingPipeline
+
+#from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+#from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
+#from cnnClassifier.pipeline.stage_04_evaluation import EvaluationPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
